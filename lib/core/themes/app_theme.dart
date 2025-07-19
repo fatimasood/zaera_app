9b04c5b741e90d0c54a8528f55b4ce3c3b4ae46b
Fatima Masood <fatimamughal103@gmail.com>
2025-07-19 22:37:50 +0500

    update signup screen

--- lib/core/themes/app_theme.dart
+++ lib/core/themes/app_theme.dart
@@ -12,45 +12,78 @@ class AppTheme {
       textTheme: TextTheme(
         displayLarge: GoogleFonts.urbanist(
           fontSize: 28,
-          fontWeight: FontWeight.bold,
+          fontWeight: FontWeight.w800,
           color: AppColors.brown,
-        ),
+        ).copyWith(inherit: true),
         displayMedium: GoogleFonts.urbanist(
           fontSize: 24,
           fontWeight: FontWeight.w600,
           color: AppColors.brown,
-        ),
+        ).copyWith(inherit: true),
         displaySmall: GoogleFonts.urbanist(
           fontSize: 20,
           fontWeight: FontWeight.w500,
           color: AppColors.brown,
-        ),
+        ).copyWith(inherit: true),
 
         bodyLarge: GoogleFonts.inter(
           fontSize: 16,
           fontWeight: FontWeight.normal,
           color: AppColors.brown,
-        ),
+        ).copyWith(inherit: true),
         bodyMedium: GoogleFonts.inter(
           fontSize: 14,
           fontWeight: FontWeight.w400,
           color: AppColors.brown,
-        ),
+        ).copyWith(inherit: true),
         bodySmall: GoogleFonts.inter(
           fontSize: 12,
           fontWeight: FontWeight.w300,
           color: AppColors.brown,
-        ),
+        ).copyWith(inherit: true),
 
         labelLarge: GoogleFonts.inter(
-          fontSize: 16,
+          fontSize: 20,
           fontWeight: FontWeight.w600,
-          color: Colors.white,
+          color: AppColors.background,
+        ).copyWith(inherit: true),
+      ),
+
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.musteredGreen,
+          foregroundColor: AppColors.background,
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 14),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+          textStyle: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w600,
+            fontFamily: "Inter",
+          ),
         ),
       ),
-      buttonTheme: ButtonThemeData(
-        buttonColor: AppColors.musteredGreen,
-        textTheme: ButtonTextTheme.primary,
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(
+          foregroundColor: AppColors.brown,
+          textStyle: GoogleFonts.inter(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: OutlinedButton.styleFrom(
+          side: BorderSide(color: AppColors.brown, width: 1.5),
+          textStyle: GoogleFonts.inter(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
       ),
     );
   }
--- lib/features/auth/signup_screen.dart
+++ lib/features/auth/signup_screen.dart
@@ -5,6 +5,49 @@ class SignupScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Text("Signup Screen"));
+    final screenHeight = MediaQuery.of(context).size.height;
+    //final screenWidth = MediaQuery.of(context).size.width;
+
+    final logoSize = screenHeight * 0.2;
+    final buttonHeight = screenHeight * 0.06; // 6% height
+
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                'lib/assets/images/logo.png',
+                width: logoSize,
+                height: logoSize,
+              ),
+              const SizedBox(height: 15),
+              Text(
+                'Create an Account',
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+              const SizedBox(height: 20),
+
+              // Input fields for email and password
+              SizedBox(
+                width: double.infinity,
+                height: buttonHeight,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // signup logic
+                  },
+                  child: Text(
+                    "Let's Go!",
+                    style: Theme.of(context).textTheme.labelLarge,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
