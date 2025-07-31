406bb84c74253dc54b4f3c3963e1c5dbb470d0bb
Fatima Masood <fatimamughal103@gmail.com>
2025-07-31 18:51:02 +0500

    bill creation.dart

--- lib/features/expense/split_bill_screen.dart
+++ lib/features/expense/split_bill_screen.dart
@@ -8,49 +8,54 @@ class SplitBillScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            context.goNamed('home');
-          },
-          icon: Icon(
-            Icons.arrow_back_ios_new,
-            color: AppColors.brown,
-            size: 24,
-          ),
-        ),
-        title: Text(
-          "Spain Trip", // Example title, replace with dynamic data
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: IconButton(
-              onPressed: () {
-                // SHOW SAME LINK AGAIN FOR SHARING WITH FRIENDS
-              },
-              icon: Icon(
-                Icons.group_add_outlined,
-                color: AppColors.tealGreen,
-                size: 24,
-              ),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              context.goNamed('home');
+            },
+            icon: Icon(
+              Icons.arrow_back_ios_new,
+              color: AppColors.brown,
+              size: 24,
             ),
           ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12),
-        child: Column(
-          children: [
-            TopBar(),
-            SizedBox(height: 15),
-            // Greetings(),
-            SizedBox(height: 20),
-            // SharedExpenses(),
-            SizedBox(height: 15),
-            // Updates(),
+          title: Text(
+            "Spain Trip",
+            textAlign:
+                TextAlign.left, // Example title, replace with dynamic data
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: IconButton(
+                onPressed: () {
+                  print("Right icon tapped!");
+                  // SHOW SAME LINK AGAIN FOR SHARING WITH FRIENDS
+                },
+                icon: Icon(
+                  Icons.group_add_outlined,
+                  color: AppColors.tealGreen,
+                  size: 24,
+                ),
+              ),
+            ),
           ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12),
+          child: Column(
+            children: [
+              TopBar(),
+              SizedBox(height: 15),
+              // Greetings(),
+              SizedBox(height: 20),
+              // SharedExpenses(),
+              SizedBox(height: 15),
+              // Updates(),
+            ],
+          ),
         ),
       ),
     );
--- lib/features/expense/widgets/top_bar.dart
+++ lib/features/expense/widgets/top_bar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+String created_date = DateTime.now().toLocal().toString().split(' ')[0];
+
 class TopBar extends StatelessWidget {
   const TopBar({super.key});
 
@@ -8,11 +10,46 @@ class TopBar extends StatelessWidget {
     return Column(
       children: [
         Row(
-          //mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            //admin + date
-            Text("Admin:"),
-            Text("Marium"),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Admin:",
+                  style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                    fontSize: 21,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "Marium",
+                  style: Theme.of(
+                    context,
+                  ).textTheme.displaySmall!.copyWith(fontSize: 17),
+                ),
+              ],
+            ),
+
+            //show the dynamic date of cretion of the bill
+            Column(
+              children: [
+                Text(
+                  "Created on: ",
+                  style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                    fontSize: 21,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  created_date,
+                  style: Theme.of(
+                    context,
+                  ).textTheme.displaySmall!.copyWith(fontSize: 17),
+                ),
+              ],
+            ),
           ],
         ),
       ],
